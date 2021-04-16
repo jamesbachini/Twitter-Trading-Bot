65e0a20067585e06502131d78853a39b4296f226
James Bachini <james@mcambridge.com>
2021-04-16 20:04:54 +0100

    Example config file

---
+++ config-example.js
@@ -0,0 +1,24 @@
+/*
+	Rename this file config.js and edit the fields below.
+*/
+config =  {
+	follow:  'elonmusk',
+	keyword: 'doge',
+	market: 'DOGE-PERP',
+	quantity: 10,
+	trailingStop: -0.026,
+	twitterAPI: {
+		"user": "@Me",
+		"consumer_key": "123",
+		"consumer_secret": "123",
+		"access_token_key": "123-abc",
+		"access_token_secret": "123",
+	},
+	ftxAPI: {
+		subAccount: `TESTACC1`,
+		apiKey : `123`,
+		apiSecret : `123-abc`,
+	},
+}
+
+module.exports = config;
